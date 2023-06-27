db9aee46b8ebf0ec2409d8c492b75ce1998400bc
sayansh3 <118974807+sayansh3@users.noreply.github.com>
2023-06-27 16:50:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+
+function setup(){
+    
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas = createCanvas(550, 500);
+    canvas.position(560,100);
+    poseNet=ml5.poseNet(video,modleLoaded);
+    poseNet.on('pose',gotPoses);
+    
+}
+noseX=0;
+noseY=0;
+difference=0;
+rightWristY=0;
+leftWristX=0;
+
+function draw(){
+   background("green");
+   textSize(difference);
+   fill('#F90093');
+   stroke('#F90093');
+   text("Sayansh",220,220);
+   document.getElementById("square_side").innerHTML = "Size of text = " + difference +"px";
+}
+
+function modleLoaded(){
+    console.log('Modle Loaded');
+
+}
+
+function gotPoses(results){
+    if (results.length>0){
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("noseX= "+noseX+"noseY= "+noseY )
+
+        rightWristX =results[0].pose.rightWrist.x;
+        leftWristX =results[0].pose.leftWrist.x;
+        difference=floor(leftWristX-rightWristX);
+
+        console.log("leftWristX= "+ leftWristX +"rightWristX= " + rightWristX )
+
+
+        
+    }
+}
